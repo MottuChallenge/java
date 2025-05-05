5d3350de251b1a49a3c3e1f02c2a43e3cf3f4328
Pedro-Henrique3216 <ph6704938@gmail.com>
2025-05-05 08:15:00 -0300

    first commit

---
+++ src/main/java/br/com/mottugrid_java/MottuGridJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.mottugrid_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MottuGridJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MottuGridJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mottugrid_java/MottuGridJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mottugrid_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MottuGridJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
